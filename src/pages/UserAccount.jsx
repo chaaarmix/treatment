421f5369bd50f30506af5f6f20f7ad92a2686c66
Alexandra <lisencoalexandra@gmail.com>
2025-02-27 13:27:35 +0300

    Логика кнопкни выхода из аккаунта

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../components/UserProvider';
 import Navbar from "../components/Navbar";
 
 const UserAccount = () => {
-
+    const { userData, setUserData } = useContext(UserContext);
+    const navigate = useNavigate();
+    const handleLogout=() =>{
+        setUserData(null);
+        localStorage.removeItem('userData');
+        navigate('/');
+    };
     return (
         <div className="App enter">
             Аккаунт
+            <button className="btn btn-primary" onClick={handleLogout}>Выйти из аккаунта</button>
             <Navbar/>
         </div>
     );
